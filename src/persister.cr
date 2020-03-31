9afb2bf0bc1f9c69267bae2fb435a03684cdde63
Kasper Jensen <kasper.jensen@solarisbank.de>
2020-03-31 09:57:47 +0200

    Use encryption

--- src/persister.cr
+++ src/persister.cr
@@ -1,16 +1,56 @@
+require "openssl"
+
 class Persister
+  property :private_key
 
   def initialize(@file_name : String)
+    @private_key = "spicy doritos reign supreme"
+  end
+
+  def encrypted_list(hash)
+    hash.map do |k, v|
+      pkey = "#{@private_key}#{k}"
+      data = aes_encrypt(v, pkey)
+    end
+  end
+
+  def aes_encrypt(data, key)
+    cipher = OpenSSL::Cipher.new("aes-128-cbc")
+    cipher.encrypt
+    cipher.key = key
+    io = IO::Memory.new
+    io.write(cipher.update(data))
+    io.write(cipher.final)
+    io.to_s.bytes.map(&.to_s(16)).join(":")
+  end
+
+  def aes_decrypt(byte_string, key)
+    byte_array = byte_string.split(":").map(&.to_i(16).to_u8)
+    byte_slice = Bytes.new(byte_array.size)
+    byte_slice.copy_from(byte_array.to_unsafe, byte_array.size)
+
+    cipher = OpenSSL::Cipher.new("aes-128-cbc")
+    cipher.decrypt
+    cipher.key = key
+
+    io = IO::Memory.new
+    io.write(cipher.update(byte_slice))
+    io.write(cipher.final)
+    io.to_s
   end
 
   def persist(q_and_a_hash)
-    File.write(file_name, q_and_a_hash.to_json)
+    list = encrypted_list(q_and_a_hash)
+    File.write(file_name, list.to_json)
   end
 
   def read_answer(file, question)
     json = JSON.parse(File.read(file))
-    any = json[question]?
-    any.as_s if any
+    encrypted_strings = json.as_a.map(&.as_s)
+    encrypted_strings.each do |cipher_text|
+      answer = aes_decrypt(cipher_text, "#{private_key}#{question}")
+      return answer
+    end
   end
 
   def file_name
